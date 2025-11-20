2f59d75996a74bcaa4ea41bc064070e5ba4e55e2
Saquib Saleem <125876033+SaquibSaleem-INPL@users.noreply.github.com>
2025-11-20 19:19:40 +0500

    Update viewSalesOrder.blade.php

--- resources/views/pages/SalesOrder/viewSalesOrder.blade.php
+++ resources/views/pages/SalesOrder/viewSalesOrder.blade.php
@@ -11,7 +11,7 @@
 .logo.logo-flex-cont{display:flex;align-items:baseline;}
 .logo-text p{color:#000;font-weight:bold;font-size:18px;}
 body{font-family:Arial,sans-serif;margin:0;padding:0;background:#fff;}
-.invoice-container{width:900px;margin:auto;padding:20px 30px;border:1px solid #000;}
+.invoice-container{width:900px;margin:auto;padding:20px 0px;}
 h2,h3,h4,p,table{margin:0;padding:0;}
 .header{display:flex;justify-content:space-between;align-items:baseline;margin-bottom:10px;}
 .logo img{width:80px;}
@@ -21,8 +21,9 @@ h2,h3,h4,p,table{margin:0;padding:0;}
 .section-table td{border:1px solid #000;padding:5px;color:#000;}
 .section-table td b{color:#000;}
 .section-table2 td{border:none !important;color:#000;}
-.item-table{width:100%;border-collapse:collapse;margin-top:15px;font-size:14px;}
-.item-table th,.item-table td{border:1px solid #000;padding:5px;text-align:center;color:#000;}
+.item-table{width:100%;border-collapse:collapse;margin-top:15px;font-size:14px;border:1px solid #000;}
+.item-table th,.item-table td{border:1px solid #000;padding:5px;text-align:center;color:#000;font-size:13px;}
+.item-table td{border:none;}
 .summary-box{width:100%;margin-top:20px;}
 .left-box{width:45%;height:120px;border:1px solid #000;display:inline-block;vertical-align:top;padding:10px;}
 .right-summary{width:50%;float:right;text-align:right;}
