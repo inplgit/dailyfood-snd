71dcba6932e9c4ba3ec90b290e4f10e63a0900b9
Saquib Saleem <125876033+SaquibSaleem-INPL@users.noreply.github.com>
2025-12-03 19:28:40 +0500

    Update multi_so_view.blade.php

--- resources/views/pages/Execution/multi_so_view.blade.php
+++ resources/views/pages/Execution/multi_so_view.blade.php
@@ -530,23 +530,12 @@ h2,h3,h4,p,table{margin:0;padding:0;}
         <div class="right-summary">
             <table class="item-table2">
                 <tr style="border-bottom:5px solid #000;">
-                <td></td>
-                <td style="text-align:left;"><u>{{ number_format($total_qty) }}</u></td>
-                <td colspan="3"></td>
-                <td></td>
-                <td style=" text-align:center;width:42px !important;"><u>{{number_format($item_total_amounts,2)}}</u></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td><u>{{ number_format($a_d_amt_total, 2) }}</u></td> 
-                <td></td>
-                <td colspan="2"><u>{{number_format($row_percentage_amount_total, 2) }}</u></td>
-                <td style="text-align:right;"><u>{{ number_format($grand_total, 2) }}</u></td>
+                <td><u>{{ number_format($total_qty) }}</u></td>
+                <td><u>{{number_format($item_total_amounts,2)}}</u></td>
+                <td><u>{{number_format($a_d_amt_total,2)}}</u></td> 
+                <td><u>{{number_format($row_percentage_amount_total, 2) }}</u></td>
+                <td><u>{{ number_format($grand_total, 2) }}</u></td>
                 </tr>
-                
-
             </table>
 
 
