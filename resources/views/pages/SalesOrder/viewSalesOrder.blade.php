a34596516d18bd8481c66257c74335b85a694476
Saquib Saleem <125876033+SaquibSaleem-INPL@users.noreply.github.com>
2025-12-03 19:55:15 +0500

    Update viewSalesOrder.blade.php

--- resources/views/pages/SalesOrder/viewSalesOrder.blade.php
+++ resources/views/pages/SalesOrder/viewSalesOrder.blade.php
@@ -514,10 +514,6 @@ h2,h3,h4,p,table{margin:0;padding:0;}
 
 
               <tr style="border-bottom:5px solid #000;">
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
                 <td><u>{{ number_format($total_qty) }}</u></td>
                 <td></td>
                 <td></td>
@@ -526,9 +522,6 @@ h2,h3,h4,p,table{margin:0;padding:0;}
                 <td></td>
                 <td></td>
                 <td><u>{{number_format($item_total_amounts,2)}}</u></td>
-                <td></td>
-                <td></td>
-                <td></td>
                 <td></td>
                 <td></td>
                    <td></td>
@@ -552,14 +545,12 @@ h2,h3,h4,p,table{margin:0;padding:0;}
                 <td></td>
                 <td></td>
                 <td></td>
-                <td></td>
-                <td></td>
                 <td><u>{{number_format($row_percentage_amount_total, 2) }}</u></td>
                  <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
-                <td></td>
+               
                 <td><u>{{ number_format($grand_total, 2) }}</u></td>
                 </tr>
 
