0498708f0188748a0fda8baac281944690b5adb5
Saquib Saleem <125876033+SaquibSaleem-INPL@users.noreply.github.com>
2025-12-04 15:16:19 +0500

    Update multi_so_view.blade.php

--- resources/views/pages/Execution/multi_so_view.blade.php
+++ resources/views/pages/Execution/multi_so_view.blade.php
@@ -514,11 +514,10 @@ h2,h3,h4,p,table{margin:0;padding:0;}
             <td>{{ number_format($pcs_per_amount, 2) }}</td>
         </tr>
         <tr>
-            <td colspan="5"> <p><b>For Inquiry/Complaint:</b><br>
+            <td style="border:1px solid #000;" colspan="5"> <p><b>For Inquiry/Complaint:</b><br>
             Contact/WhatsApp: 0300-0813906<br>
             Email us at: support@dailyfoodindustries.com
         </p></td>
-        <td></td>
         <td></td>
         <td><u>{{ number_format($total_qty) }}</u></td>
               <td></td>
@@ -530,7 +529,7 @@ h2,h3,h4,p,table{margin:0;padding:0;}
                    <td><u>{{number_format($a_d_amt_total,2)}}</u></td> 
                      <td></td>
                         <td><u>{{number_format($row_percentage_amount_total, 2) }}</u></td>
-                         <td></td>
+                         
                         <td><u>{{ number_format($grand_total, 2) }}</u></td>
 
         </tr>
