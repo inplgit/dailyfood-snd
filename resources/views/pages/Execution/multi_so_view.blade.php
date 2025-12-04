b9dba6821461b03179c5347a994cf935a9f5057e
Saquib Saleem <125876033+SaquibSaleem-INPL@users.noreply.github.com>
2025-12-04 14:48:42 +0500

    Update multi_so_view.blade.php

--- resources/views/pages/Execution/multi_so_view.blade.php
+++ resources/views/pages/Execution/multi_so_view.blade.php
@@ -49,7 +49,7 @@ h2,h3,h4,p,table{margin:0;padding:0;}
 .item-table{width:100%;border-collapse:collapse;margin-top:15px;font-size:14px;border:1px solid #000;}
 .item-table th,.item-table td{border:1px solid #000;padding:5px;text-align:center;color:#000;font-size:13px;}
 .item-table td{border:none;}
-.item-table2{border-collapse:collapse;margin-top:15px;font-size:14px;border:none;margin-bottom:12px;    width: 100%;}
+.item-table2{border-collapse:collapse;margin-top:15px;font-size:14px;border:none;margin-bottom:12px;}
 .item-table2 th,.item-table2 td{border:1px solid #000;padding:5px;text-align:center;color:#000;font-size:13px;}
 .item-table2 td{border:none;}
 .summary-box{width:100%;margin-top:10px;display:flex;align-items:flex-start;gap:16px;}
