cf3e82243169139df768bd3ed0037f0213f86cb6
shariqiqbalinnovative <shariq.m.iqbal@innovative-net.com>
2025-12-19 10:04:44 +0500

    added route ajax tansfer

--- resources/views/pages/Routes/Route/RouteTransferAjax.blade.php
+++ resources/views/pages/Routes/Route/RouteTransferAjax.blade.php
@@ -2,9 +2,11 @@
 use App\Helpers\MasterFormsHelper;
 $master = new MasterFormsHelper();
 ?>
+
 <form method="post" action="{{ route('route.transfer_store') }}">
-    @method('PUT')
     @csrf
+    @method('PUT')
+
     <div class="table-responsive">
         <table class="table table-bordered">
             <thead>
@@ -12,60 +14,54 @@ $master = new MasterFormsHelper();
                     <th>Sr No</th>
                     <th>Route Name</th>
                     <th>Distributor</th>
-                    <th>Order Booker</th>
+                    <th>Order Booker (TSO)</th>
                 </tr>
             </thead>
-            <tbody id="data">
+            <tbody>
                 @foreach ($tso as $key => $row)
+                    @php
+                        $selectedTsos = explode(',', $row->tso_ids);
+                    @endphp
                     <tr>
-                        <td>{{ ++$key }}</td>
+                        <td>{{ $key + 1 }}</td>
                         <td>{{ $row->route_name }}</td>
-                        <td>
-                            <select  onchange="get_tso2(this)" class="form-control select2 distributor_ids" name="distributor_ids[{{ $key }}]">
-                                <option value="">select</option>
-                                @foreach ( $master->get_all_distributors() as $row1 )
-                                   <option value="{{ $row1->id }}" {{$row->distributor_id == $row1->id  ? 'selected' : ''}}>{{ $row1->distributor_name }}</option>
-                                @endforeach
-                            </select>
-                        </td>
-                        <!-- <td>
-                            <select class="form-control select2 tso_ids" id="tso_ids" name="tso_ids[{{ $key }}]"
-                                required>
-                                <option value="">select</option>
-                                @foreach ( $master->get_all_tso_by_distributor_id($row->distributor_id) as $row2 )
-                                    <option value="{{ $row2->id }}" {{$row->tso_id == $row2->id ? 'selected' : ''}}>{{ $row2->name }}</option>
-                                @endforeach
-                                {{-- @foreach ($master->get_all_tso() as $row1)
-                                    <option value="{{ $row1->id }}">{{ $row1->name }}</option>
-                                @endforeach --}}
-                            </select>
-                            <input type="hidden" name="ids[{{ $key }}]" value="{{ $row->id }}" />
-                        </td> -->
-                        <td>
-    <select 
-        class="form-control select2 tso_ids"
-        name="tso_ids[{{ $key }}][]"
-        multiple
-        required
-    >
-        @foreach ($master->get_all_tso_by_distributor_id($row->distributor_id) as $row2)
-            <option value="{{ $row2->id }}"
-                {{ $row->tso_id == $row2->id ? 'selected' : '' }}>
-                {{ $row2->name }}
+
+                       <td>
+    <select class="form-control select2 distributor_ids"
+            onchange="get_tso2_tasfer(this)"
+            name="distributor_ids[{{ $key }}]">
+        <option value="">Select</option>
+        @foreach ($master->get_all_distributors() as $d)
+            <option value="{{ $d->id }}"
+                {{ $row->distributor_id == $d->id ? 'selected' : '' }}>
+                {{ $d->distributor_name }}
+            </option>
+        @endforeach
+    </select>
+</td>
+
+<td>
+    <select class="form-control select2 tso_ids"
+            name="tso_ids[{{ $key }}][]"
+            multiple required>
+        @foreach ($master->get_all_tso_by_distributor_id($row->distributor_id) as $t)
+            <option value="{{ $t->id }}"
+                {{ in_array($t->id, $selectedTsos) ? 'selected' : '' }}>
+                {{ $t->name }}
             </option>
         @endforeach
     </select>
 
-    <input type="hidden" name="ids[{{ $key }}]" value="{{ $row->id }}" />
+    <input type="hidden" name="ids[{{ $key }}]" value="{{ $row->id }}">
 </td>
+
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
-    </br>
-    <div class="col-12">
-        <button type="submit" class="btn btn-primary mr-1">Transfer</button>
 
+    <div class="mt-3">
+        <button type="submit" class="btn btn-primary">Transfer</button>
     </div>
 </form>
