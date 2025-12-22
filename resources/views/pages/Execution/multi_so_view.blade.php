13e181f653da58699017fbadb0cacbaf7a67467c
Saquib Saleem <125876033+SaquibSaleem-INPL@users.noreply.github.com>
2025-12-22 19:08:57 +0500

    Update multi_so_view.blade.php

--- resources/views/pages/Execution/multi_so_view.blade.php
+++ resources/views/pages/Execution/multi_so_view.blade.php
@@ -534,6 +534,10 @@ h2,h3,h4,p,table{margin:0;padding:0;}
                 <td></td>
                 <td></td>
                 <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
                 <td><u>{{ number_format($total_qty) }}</u></td>
                 <td></td>
                 <td></td>
@@ -541,11 +545,7 @@ h2,h3,h4,p,table{margin:0;padding:0;}
                 <td></td>
                 <td></td>
                 <td></td>
-                <td></td>
                 <td><u>{{number_format($item_total_amounts,2)}}</u></td>
-                <td></td>
-                <td></td>
-                <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
